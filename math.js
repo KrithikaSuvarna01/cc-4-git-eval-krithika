0ea038819fe5eb062f81142dfdcba8a55a37b0a2
KrithikaSuvarna01 <krithika.suvarna@codecraft.co.in>
2023-08-16 11:19:44 +0530

    Feat: Implemeneted Subraction Feature to the math.js

--- math.js
+++ math.js
@@ -8,3 +8,13 @@
 function add(number1,number2){
     return number1+number2
 }
+
+/**
+ * Function subtracts number2 from number1 and returns the result 
+ * @param {*} number1 
+ * @param {*} number2 
+ * @returns the difference between 2 numbers
+ */
+function subtract(number1,number2){
+    return number1-number2
+}
