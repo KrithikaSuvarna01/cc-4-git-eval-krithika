954eea576ee076c0f60203cb76bf0533356427fc
KrithikaSuvarna01 <krithika.suvarna@codecraft.co.in>
2023-08-16 11:22:29 +0530

    Feat: Added Javascript documentation to Subtraction Feature inside math.js file

--- math.js
+++ math.js
@@ -11,9 +11,9 @@ function add(number1,number2){
 
 /**
  * Function subtracts number2 from number1 and returns the result 
- * @param {*} number1 
- * @param {*} number2 
- * @returns the difference between 2 numbers
+ * @param {*} number1 is the number which is being subtracted by
+ * @param {*} number2 is the number which is used to subtract
+ * @returns the difference between 2 numbers 
  */
 function subtract(number1,number2){
     return number1-number2
